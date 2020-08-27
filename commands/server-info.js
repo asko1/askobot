a3957d2d38950aed440482afd396be51edf5b05f
asko1 <oneasko@gmail.com>
2020-08-27 22:28:27 +0300

    finishing server-info (for now)

--- commands/server-info.js
+++ commands/server-info.js
@@ -1,15 +1,47 @@
+const moment = require("moment");
+
 exports.run = (client, message, args) => {
     server = message.guild;
+    var roles = "";
+    var chnlCount = 0;
+    var botCount = 0;
+
+    // Gets each
+    server.roles.cache.forEach(role => {
+        roles += `${role} `
+    });
+
+    // Gets amount of channels without category type
+    server.channels.cache.forEach(chnl => {
+        if (chnl.type == "category") return;
+        chnlCount += 1;
+    })
+
+    // Gets amount of bots
+    server.members.cache.forEach(member => {
+        if (member.user.bot == true) botCount += 1;
+    })
+    
+    // Bot or bots?
+    if (botCount == 1) {
+        botCountStr = botCount + " bot";
+    } else botCountStr = botCount + " bots";
+
     var embed = new client.Discord.MessageEmbed()
         .setAuthor(server.name, server.iconURL())
         .setTitle(server.id)
         .setThumbnail(server.iconURL())
         .setColor(client.config.color)
         .addFields(
-            {name: "Verification Level", value: "ID:" + server.verificationLevel, inline:true},
+            {name: "Owner", value: server.owner.user.tag},
+            {name: "Creation Date", value: moment(server.createdAt).format(client.timeFormat)},
+            {name: "Verification Level", value: server.verificationLevel, inline:true},
             {name: "Explicit Content Filter Level", value: server.explicitContentFilter, inline:true},
-            {}
-        )
+            {name: '\u200B', value: '\u200B' },
+            {name: "Members", value: server.members.cache.size + ` (${botCountStr})`, inline: true},
+            {name: "Channels", value: chnlCount, inline: true},
+            {name: "Roles", value: roles}
+        );
     message.channel.send(embed).catch(console.error);
 }
 exports.help = {
