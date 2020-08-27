3e3fa979ac04cbcee9c6ab43372e0fe3068a0a5a
asko1 <oneasko@gmail.com>
2020-08-27 21:08:53 +0300

    trycatch for setconf

--- commands/setconf.js
+++ commands/setconf.js
@@ -14,12 +14,16 @@ exports.run = (client, message, args, guildConf) => {
     if(!client.settings.has(message.guild.id, prop)) {
         return message.channel.send("This key is not in the configuration.");
     }
-    
-    // Changes config
-    client.settings.set(message.guild.id, value.join(" "), prop);
 
-    // Send confirmation
-    message.channel.send(`Guild configuration item ${prop} has been changed to:\n\`${value.join(" ")}\``);
+    // Changes config
+    try {
+        client.settings.set(message.guild.id, value.join(" "), prop);
+
+        // Send confirmation
+        message.channel.send(`Guild configuration item ${prop} has been changed to:\n\`${value.join(" ")}\``);
+    } catch (error) {
+        return client.logger.error(`${error}`);
+    }
 };
 
 exports.help = {
