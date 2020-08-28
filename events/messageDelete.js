fe79cd35837b40561d0cd3517d2ffa5ce9ce4622
asko1 <oneasko@gmail.com>
2020-08-28 09:31:42 +0300

    channel

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -3,6 +3,8 @@ module.exports = (client, message) => {
     var embed = new client.Discord.MessageEmbed()
         .setColor(client.color)
         .setAuthor(message.author.tag, message.author.avatarURL())
-        .addFields({name: "Deleted Message", value: message.content});
+        .addFields(
+        {name: "Deleted Message", value: message.content},
+        {name: "Channel", value: `${message.channel}`});
         client.channels.cache.get(guildConf.modLogChannel).send(embed).catch(console.error);
 }
