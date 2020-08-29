20ce233a1098cae4541068a92c9d7f4e4f412377
asko1 <oneasko@gmail.com>
2020-08-29 09:42:13 +0300

    wack

--- commands/showconf.js
+++ commands/showconf.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args, guildConf) => {
     let configProps = Object.keys(guildConf).map(prop => {
         return `${prop}:  ${guildConf[prop]}`;
-      });
+    });
       var embed = new client.Discord.MessageEmbed()
         .setColor(client.config.color)
         .addFields({name: "Settings", value: configProps.join("\n")});
