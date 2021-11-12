8e950ad4f8d6d44ad5fdf64cc9bcd762774560b9
asko1 <oneasko@gmail.com>
2021-11-12 21:48:14 +0200

    config.js mess

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-"defaultSettings" : { //defaults for server configs
-    "prefix": "!",
-    "modLogChannel": ""
-  },
-"color": "3DF5FF" // color for embeds
-};
-
-module.exports = config;
